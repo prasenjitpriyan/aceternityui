25c748563b6501bbe8278c9af2fbfe23c6af134d
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-12 20:54:23 +0530

    Herosection completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import React from "react";
 
 const Home: React.FC = () => {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-white text-4xl font-bold text-center">Home</h1>
+      <HeroSection />
     </main>
   );
 };
